835f05524bf4b9b4f0d2c71241eb6f75ecb95b03
Brian Steffens <briansteffens@gmail.com>
2016-05-05 20:41:37 -0400

    Allow multiple sessions to coexist

--- ws/cli.py
+++ ws/cli.py
@@ -11,10 +11,45 @@ usage:
 
 import os
 import sys
+import random
 from subprocess import call
 
 from docopt import docopt
 args = docopt(__doc__)
+
+# Make a random ID, 10 chars long, using 0-9, A-Z, a-z, -, and _ as digits
+def generate_ws_id():
+    ret = ''
+
+    for i in range(10):
+        c = random.randrange(0, 65)
+
+        # 0-9
+        if c < 10:
+            ret += str(c)
+            continue
+
+        c -= 10
+
+        # A-Z
+        if c < 26:
+            ret += chr(c + 65)
+            continue
+
+        c -= 26
+
+        # a-z
+        if c < 26:
+            ret += chr(c + 97)
+            continue
+
+        # - and _
+        if c == 0:
+            ret += "-"
+        else:
+            ret += "_"
+
+    return ret
 
 templates = os.path.split(os.path.realpath(__file__))[0]+"/templates/"
 
@@ -32,7 +67,14 @@ if args["init"]:
     call(["cp -r "+template+"/* ./"],shell=True)
     call(["cp -r "+template+"/.ws ./"],shell=True)
 
+    with open('.ws/id', 'w') as f:
+        f.write(generate_ws_id())
+
+
 if args["open"] or args["init"]:
+    with open('.ws/id', 'r') as f:
+        os.environ['WS_SESSION'] = 'ws-' + f.read()
+
     call(["./.ws/open"])
 
 else:
